4b5f67334cfc78d1cd1a0c2e698c5e6a60ef8c60
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-11-21 16:03:22 +0900

    useEffect Cleanup.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byFn() {
+    console.log("Bye :(");
+  }
+
+  function hiFn() {
+    console.log("Hi :)");
+    return byFn;
+  }
+
+  useEffect(hiFn, []);
+  // useEffct의 Clean Up기능. return 하면 파괴될때 불려짐. 자주 쓰이진 않는듯.
+  // useEffect(() => {
+  //   console.log("created :)");
+  //   return () => console.log("destory :(");
+  // }, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  // setValue의 함수적 갱신. setValue의 prev는 예전값이 세팅 되어있다. 여기선 counter의 값일듯.
-  // 예전 값인 prev의 값을 받아서 그 값에다가 1을 더해라는 의미.
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when couner changes.");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("I run when counter & keyword changes.");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  // useEffect(() => {}, []);
+  // useEffect(() => {
+  //   console.log(showing);
+  // }, [showing]);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-      {/* <Button text={"Continue"} /> */}
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

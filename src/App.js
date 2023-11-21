c24ba28437425e0298badc2c5c2e3955e13e5241
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-11-21 14:31:12 +0900

    Effects first study.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  // setValue의 함수적 갱신. setValue의 prev는 예전값이 세팅 되어있다. 여기선 counter의 값일듯.
+  // 예전 값인 prev의 값을 받아서 그 값에다가 1을 더해라는 의미.
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render");
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      {/* <Button text={"Continue"} /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // creat-react-app로 설치했을때 기본적으로 생성되는 테그임. 이게 써있으면 렌더링이 두번 됨.
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );

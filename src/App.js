c9dd3ed50481d8f14ba5d31ffa0dc267490d6e77
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-11-21 16:15:06 +0900

    practice movie app.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,7 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function byFn() {
-    console.log("Bye :(");
-  }
-
-  function hiFn() {
-    console.log("Hi :)");
-    return byFn;
-  }
-
-  useEffect(hiFn, []);
-  // useEffct의 Clean Up기능. return 하면 파괴될때 불려짐. 자주 쓰이진 않는듯.
-  // useEffect(() => {
-  //   console.log("created :)");
-  //   return () => console.log("destory :(");
-  // }, []);
-
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  // useEffect(() => {}, []);
-  // useEffect(() => {
-  //   console.log(showing);
-  // }, [showing]);
-
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

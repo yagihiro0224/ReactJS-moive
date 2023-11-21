aecfa4784eb5f00c0fd892ae07bd3ddb0676d009
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-11-21 18:56:33 +0900

    Coin Tracker.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+//https://api.coinpaprika.com/v1/tickers
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  function onChange(event) {
+    setMyMoney(event.target.value);
+  }
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <input
+        onChange={onChange}
+        value={myMoney}
+        type="number"
+        placeholder="Input your USD"
+      />
+      <h3>USD = </h3>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}) : {myMoney / coin.quotes.USD.price}
+              {coin.symbol}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }

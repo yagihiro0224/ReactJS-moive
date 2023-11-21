bf822c44f68386f5b351507cc8211c18c735be9d
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-11-21 16:39:54 +0900

    ToDo List first.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        ></input>
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;

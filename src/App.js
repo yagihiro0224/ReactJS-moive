77bf9f2e2d8060ec8d7f9a01707df8a3a3e81e07
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-11-22 13:06:14 +0900

    change before ".then".

--- src/App.js
+++ src/App.js
@@ -1,47 +1,27 @@
 import { useEffect, useState } from "react";
-//https://api.coinpaprika.com/v1/tickers
+//const [,] = useState();
+//useEffect(() => {}, []);
+/*
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+*/
+const url =
+  "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [myMoney, setMyMoney] = useState([]);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch(url)
       .then((response) => response.json())
       .then((json) => {
-        setCoins(json);
+        setMovies(json.data.movies);
         setLoading(false);
       });
   }, []);
-
-  function onChange(event) {
-    setMyMoney(event.target.value);
-  }
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <input
-        onChange={onChange}
-        value={myMoney}
-        type="number"
-        placeholder="Input your USD"
-      />
-      <h3>USD = </h3>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}) : {myMoney / coin.quotes.USD.price}
-              {coin.symbol}
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
